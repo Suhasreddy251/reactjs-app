0ae53c197456e4b55206666fde278b70d78136da
Suhasreddy251 <139577487+Suhasreddy251@users.noreply.github.com>
2024-08-11 18:29:50 +0530

    Create logging.js

---
+++ src/Components/logging.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import log from './logger';
+
+function App() {
+  log.info('App component rendered');
+
+  return (
+    <div>
+      {/* Your component code */}
+    </div>
+  );
+}
+
+export default App;
